512b4b104434a028d568e4794ddeaf46af1d29d6
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-05-19 18:49:08 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,15 @@
+from .nodes import LoadMoviiGenModel, Prompt, MoviiGen
 
+NODE_CLASS_MAPPINGS = {
+    "LoadMoviiGenModel": LoadMoviiGenModel,
+    "Prompt": Prompt,
+    "MoviiGen": MoviiGen,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LoadMoviiGenModel": "Load MoviiGen Model",
+    "Prompt": "Prompt",
+    "MoviiGen": "MoviiGen",
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
